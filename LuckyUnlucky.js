14fe6d838a3ebca55a001f2f875ae5463428bb63
Brandon Xavier <brandonxavier421@gmail.com>
2013-06-22 18:23:06 -0500

    GPLv3 Licensing Applied

--- LuckyUnlucky.js
+++ LuckyUnlucky.js
@@ -1,9 +1,34 @@
+
+/**
+ * Created with JetBrains WebStorm
+ * User: brandonxavier
+ * Date: 6/20/13
+ *
+
+ Copyright 2013 Brandon Xavier (brandonxavier421@gmail.com)
+
+ This file is part of ${PROJECT_NAME}.
+
+ ${PROJECT_NAME} is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ${PROJECT_NAME} is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with ${PROJECT_NAME}.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 //
 // goodbadluck.js
 //
 // Version:     1.1
-// Author:      Brandon Xavier (username brandonxavier)
-//
+
 // The standard "guess a number" game with a slight twist:  If an unlucky
 // number is guessed, the numbers start over from scratch.  Additionally,
 // unless the "lucky" number is equal to the min, the unlucky number will
